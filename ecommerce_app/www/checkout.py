69dd9c9b564c700bbce387584a62d1365a127845
ahmedatef9693 <ahmedatef93legend@gmail.com>
2024-02-09 01:40:26 +0200

    login guest issue and redirect to checkout

---
+++ ecommerce_app/www/checkout.py
@@ -0,0 +1,11 @@
+import frappe
+
+
+
+
+
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = f"/login?redirect-to={frappe.request.url}"
+        raise frappe.Redirect
