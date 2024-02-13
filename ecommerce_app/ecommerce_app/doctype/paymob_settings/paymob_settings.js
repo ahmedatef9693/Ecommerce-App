acbb995bc906286643dce532309d9049067846ae
ahmedatef9693 <ahmedatef93legend@gmail.com>
2024-02-14 01:44:42 +0200

    paymob api integration doctypes addition

---
+++ ecommerce_app/ecommerce_app/doctype/paymob_settings/paymob_settings.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Dev.AhmedAtef and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("PayMob Settings", {
+  refresh: function (frm) {
+    // frm.add_custom_button(__("Sync Products Now"), () => {
+    // });
+  },
+});
