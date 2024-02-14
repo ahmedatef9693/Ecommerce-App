fe0ad99c5f5426a96e9d42dc9e7bf755fa1afeb1
ahmedatef9693 <ahmedatef93legend@gmail.com>
2024-02-14 17:47:01 +0200

    getting colors based on product size for checkout

--- ecommerce_app/utils/order_api.py
+++ ecommerce_app/utils/order_api.py
@@ -1,7 +1,28 @@
 import frappe
-
+import json
 
 @frappe.whitelist()
 def handle_checkout_submit(product_name =None,order_data = None):
     print(f"\n\n{product_name}\n\n")
     print(f"\n\n{order_data}\n\n")
+
+
+@frappe.whitelist()
+def get_colors_of_size(product_size,product_name):
+    colors = [
+        current_color.color for current_color in frappe.db.sql("""
+                                                               select 
+                                                                    color 
+                                                               from 
+                                                                    `tabStore Product Variant` 
+                                                               where 
+                                                                    size = %(size)s 
+                                                               and 
+                                                                    parent = %(parent)s
+                                                               """,values={
+                                                                    'size':product_size,
+                                                                    'parent':product_name
+                                                                    },
+                                                                as_dict=True)]
+    return colors
+    
--- ecommerce_app/www/checkout.js
+++ ecommerce_app/www/checkout.js
@@ -5,7 +5,7 @@ document.querySelector("#payment_form").addEventListener("submit", (event) => {
   frappe.call({
     method: "ecommerce_app.utils.order_api.handle_checkout_submit",
     args: {
-      product_name:document.querySelector("#product_name").textContent.split(":")[1],
+      product_name:get_product_name(),
       order_data: form_data_object,
     },
     callback: (r) => {
@@ -20,3 +20,33 @@ document.querySelector("#payment_form").addEventListener("submit", (event) => {
   });
   console.log("hello submision of form");
 });
+
+
+document.getElementById("product_size").addEventListener("change",function(event){
+  let product_name = get_product_name();
+  let product_size = this.value;
+
+  frappe.call({
+    method: "ecommerce_app.utils.order_api.get_colors_of_size",
+    args: {
+      product_size : product_size,
+      product_name : product_name
+    },
+    callback: (r) => {
+      let select_element = document.querySelector("#product_color");
+      select_element.innerHTML = ""
+      let option_element = ``;
+      r.message.forEach(current_color => {
+        option_element+=`<option value="${current_color}">${current_color}</option>`
+        select_element.innerHTML = option_element;
+      });
+    },
+    error: (r) => {
+      frappe.throw("Failed to get colors")
+    },
+  });
+})
+
+function get_product_name(){
+  return document.querySelector("#product_name").textContent.split(":")[1].trim();
+}
