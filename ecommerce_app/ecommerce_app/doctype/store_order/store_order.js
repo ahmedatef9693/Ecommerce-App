4ee988fbe012958e323765139a95ceefc9512348
ahmedatef9693 <ahmedatef93legend@gmail.com>
2024-12-16 23:21:13 +0200

    payment transaction

--- ecommerce_app/ecommerce_app/doctype/store_order/store_order.js
+++ ecommerce_app/ecommerce_app/doctype/store_order/store_order.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2024, Dev.AhmedAtef and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Store Order', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Store Order", {
+  refresh: function (frm) {
+    if (frm.doc.status == "Pending") {
+      frm.add_custom_button(__("Pay Order"), function () {
+        frappe.call({
+          method: "ecommerce_app.utils.payment_api.payment_transacton",
+          args: { order_name: frm.doc.name },
+          callback: (r) => {
+            frappe.show_alert({
+              message: "Order Paied Successfully!",
+              indicator: "green",
+            });
+          },
+          error: (r) => {
+            // on error
+          },
+        });
+      });
+    }
+  },
 });
--- ecommerce_app/utils/payment_api.py
+++ ecommerce_app/utils/payment_api.py
@@ -1,6 +1,6 @@
 import json
 from ecommerce_app.utils.tokens import get_token
-
+import frappe
 
 def get_payments_data(token,order_id,integration_id,order_data,store_order_doc,ecommerce_setting_doc):
 	payment_data = {
@@ -30,3 +30,21 @@ def get_payments_data(token,order_id,integration_id,order_data,store_order_doc,e
 	payment_data = json.dumps(payment_data)
 	payment_access_token = get_token("payment_access_token",payment_data = payment_data)
 	return payment_access_token
+
+@frappe.whitelist()
+def payment_transacton(order_name):
+	store_order = frappe.get_cached_doc('Store Order',order_name)
+	try:
+		si_doc = frappe.new_doc('Sales Invoice')
+		si_doc.customer = 'ahmed'
+		si_doc.company = 'A&A'
+		si_doc.custom_order_id = store_order.order_id
+		si_doc.append('items',{})
+		si_doc.items[0].item_code = store_order.product
+		si_doc.items[0].qty = store_order.quantity
+		si_doc.items[0].rate = store_order.retail_price
+		si_doc.insert()
+		si_doc.submit()
+		frappe.db.set_value("Store Order",store_order.name,'status','Delivered') 
+	except Exception as e:
+		frappe.log_error("Error Occured in Creating Sales Invoice!")
